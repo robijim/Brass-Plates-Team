82ed35bf4e1b7632df48ccdee6e89fe2d300aea2
James Robison <jimrsara1975@gmail.com>
2016-11-10 20:03:12 -0700

    ViewInterface and View Class

---
+++ BrassPlateTeam/src/citbyui/cit260/brassPlatesTeam/view/View.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.brassPlatesTeam.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jamesrobison
+ */
+public abstract class View implements ViewInterface{
+    
+    protected String displayMessage;
+
+    public View() {
+    }
+    
+    public View(String message) {
+        this.displayMessage = message;
+    }
+    
+    @Override
+    public void display(){
+        boolean done = false;
+        do{
+            //prompt for and get players name
+            String value = this.getInput();
+            if (value.toUpperCase().equals("Q")) //user wants to quit
+                return; //exit the View
+            
+            //do the requested action and display the next view
+            done = this.doAction(value);
+            
+        } while (!done);
+    }
+    
+    @Override
+    public String getInput() {
+          Scanner keyboard = new Scanner(System.in);
+      String value = null; //value to be returned
+      boolean valid = false; //initialize to not valid
+      
+      while (!valid) { //loop while an invalid value is entered
+          System.out.println("\n" + this.displayMessage);          
+          value = keyboard.nextLine(); //get next line typed on keyboard
+          value = value.trim(); //trim off leading and trailing blanks
+          if (value.length() < 1) { // value is blank
+              System.out.println("\nInvalid value: value can not be blank");
+              continue;
+          }
+          
+          break; //end the loop
+      }
+      
+      return value; //return the value entered
+    }
+}
