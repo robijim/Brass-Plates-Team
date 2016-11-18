a4cebc14a7a611826e02be713d544e710e0ab392
James Robison <jimrsara1975@gmail.com>
2016-11-17 18:32:24 -0700

    Convert Actor Class to Enum Class

--- BrassPlateTeam/src/byui/cit260/brassPlatesTeam/model/Actor.java
+++ BrassPlateTeam/src/byui/cit260/brassPlatesTeam/model/Actor.java
@@ -5,33 +5,34 @@
  */
 package byui.cit260.brassPlatesTeam.model;
 
+import java.awt.Point;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Actor implements Serializable {
+public enum Actor implements Serializable {
+    
+    Lehi("He is the prophet and leader of the family."),
+    Nephi("Son of Lehi, and obedient son."),
+    Sam("Son of Lehi, and faithful brother of Nephi."),
+    Laman("Oldest son of Lehi, complainer, and not a faithful brother or son."),
+    Lemuel("Sam and Nephi's 2nd oldest brother, not faithful, and follows Laman's lead."),
+    Laban("Keeper and protector of the Brass Plates."),
+    Zoram("Laban's servant in the treasury, and eventually a faithful friend to Nephi and his family.");
+    
+    private final String description;
+    private final Point coordinates;
 
-    private String name;
-    private String description;
-    private String coordinates;
-
-    public Actor() {
-        
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    Actor(String description) {
+        this.description = description;
+        coordinates = new Point(1,1);
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public Point getCoordinates() {
+        return coordinates;
     }
 }
 
